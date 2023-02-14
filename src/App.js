39352f345f7186a8d839af5748cbba2d11002af3
jahidhasan8 <jahidhasan1932@gmail.com>
2023-02-14 11:54:48 +0600

    modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+
 import { RouterProvider } from "react-router-dom";
+import ProductProvider from "./context/ProductProvider";
 import routes from "./routes/routes";
 
+
 function App() {
+  
+
+  
   return (
     <div>
+      
+      <ProductProvider>
+
       <RouterProvider router={routes} />
+      </ProductProvider>
+    
     </div>
   );
 }
